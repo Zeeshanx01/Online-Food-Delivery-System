e8346f8d7f0bb0d05361c678b99174ba26e13953
Zeeshanx01 <zeeshanmunir13579@gmail.com>
2024-12-11 19:22:57 +0500

    24-12-11 Food Item CRUD APIs created

---
+++ pages/api/fooditemCRUD/create.js
@@ -0,0 +1,62 @@
+// pages/api/create.js
+// 'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
+import { createConnection } from 'mysql2/promise';
+// import bcrypt from 'bcrypt';
+
+// Function to create a MySQL connection
+async function connectToDatabase() {
+  return createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+}
+
+export default async function handler(req, res) {
+  // const { data: session } = useSession()
+
+  if (req.method !== 'POST') {
+    console.log(req);
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+
+  // const { name, description, price, image } = req.body;
+  // console.log(req.body);
+
+
+  const name = "Best Bite 3"
+  const description = "1"
+  const price = 1300
+  const image = 'restaurant/abc2.jpg'
+  const res_id = 1
+  
+  if (!name || !description || !price || !image || !res_id) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+  try {
+    // const hashedPassword = await bcrypt.hash(password, 10); // Uncomment to hash password
+
+    // Connect to the database
+    const connection = await connectToDatabase();
+
+    // Execute a query to insert data into the "users" table
+    const [result] = await connection.execute(
+      'INSERT INTO food_items (name, description, price, image, res_id) VALUES (?, ?, ?, ?,?)',
+      [name, description, price, image, res_id]
+    );
+
+    // Close the database connection
+    await connection.end();
+
+    // Respond with the created user data
+    res.status(201).json({ id: result.insertId, message: 'Food Item Added successfully' });
+  } catch (error) {
+    console.error('Error:', error.message); // Logs the error message
+    res.status(500).json({ error: error.message || 'Internal Server Error' });
+  }
+
+}
---
+++ pages/api/fooditemCRUD/update.js
@@ -0,0 +1,60 @@
+import { createConnection } from 'mysql2/promise';
+
+// Function to create a MySQL connection
+async function connectToDatabase() {
+  return createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+}
+
+// Update user API route
+export default async function handler(req, res) {
+  if (req.method !== 'PUT') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  // const { name, description, price, image } = req.body;
+  // console.log(req.body);
+
+  const id = 1
+  const name = "Burger 1"
+  const description = "lorem"
+  const price = 1300
+  const image = 'restaurant/abc2.jpg'
+  const res_id = 1
+
+
+  if (!id || !name || !description || !price || !image || !res_id) {
+    return res.status(400).json({ error: 'id, name, status and image are required in the request body.' });
+  }
+
+
+  try {
+    // Connect to the database
+    const connection = await connectToDatabase();
+
+    // Execute a query to update the user in the "users" table
+    const [result] = await connection.execute(
+      'UPDATE food_items SET name = ?, description = ?, price = ?, image = ?, res_id = ? WHERE id = ?',
+      [name, description, price, image, res_id, id]
+    );
+
+    // Close the database connection
+    await connection.end();
+
+    // Check if the user was updated successfully
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: 'Food Item not found.' });
+    }
+
+    // Respond with a success message
+    res.status(200).json({ message: 'Food item updated successfully' });
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
